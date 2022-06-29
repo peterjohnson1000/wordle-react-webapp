ad56d56d2bed2cd8069c34972e32293d2b676330
Peter Johnson <peterj1298@gmail.com>
2022-06-29 22:19:09 +0530

    structure defined

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const API_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>Wordle</nav>
     </div>
   );
 }
